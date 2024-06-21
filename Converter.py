2ca17aa5fcceac93675457f4e8ff2559576db264
mortbros <sandromort@gmail.com>
2024-06-21 14:35:32 +1000

    Add structure to Converter class

--- Converter.py
+++ Converter.py
@@ -2,17 +2,15 @@ import comtypes.client
 import os
 from Config import Config
 
-supported_input_types = ["pdf", "html", "pptx", "docx"]
-supported_output_types = ["pdf", "html"]
-
-config = Config()
-
 
 class Converter:
 
     def __init__(self, output_file_type):
         self.created_files = []
         self.output_file_type = output_file_type
+        self.supported_input_types = ["pdf", "html", "pptx", "docx"]
+        self.supported_output_types = ["pdf", "html"]
+        self.config = Config()
 
     def convert(self, input_file_path, output_file_path):
         # convert input and output to absolute paths if not already
@@ -23,30 +21,40 @@ class Converter:
         if output_file_path[-3:] != self.output_file_type:
             output_file_path = f"{output_file_path}.{self.output_file_type}"
 
-        input_ext = input_file_path.split(".")[-1]
-        output_ext = output_file_path.split(".")[-1]
+        input_type = input_file_path.split(".")[-1]
+        output_type = output_file_path.split(".")[-1]
 
-        if input_ext not in supported_input_types:
+        if input_type not in self.supported_input_types:
             print(
-                f"Unsupported input file type '{input_ext}' (supported types are {', '.join(supported_input_types)})"
+                f"Unsupported input file type '{input_type}' (supported types are {', '.join(self.supported_input_types)})"
             )
             return False
-        elif output_ext not in supported_output_types:
+        elif output_type not in self.supported_output_types:
             print(
-                f"Unsupported output file type '{output_ext}' (supported types are {', '.join(supported_input_types)})"
+                f"Unsupported output file type '{output_type}' (supported types are {', '.join(self.supported_input_types)})"
             )
             return False
 
-        if input_ext == "pdf" and output_ext == "html":
+        if input_type == "pdf" and output_type == "html":
             self.PDF_to_HTML(input_file_path, output_file_path)
-        elif input_ext == "pdf" and output_ext == "html":
+        elif input_type == "pdf" and output_type == "docx":
+            self.PDF_to_DOCX(input_file_path, output_file_path)
+        elif input_type == "docx" and output_type == "html":
+            self.DOCX_to_HTML(input_file_path, output_file_path)
+        elif input_type == "pptx" and output_type == "html":
             self.PPTX_to_PDF(input_file_path, output_file_path)
 
     def PDF_to_HTML(self, input_file_path, output_file_path):
         os.system(
-            f'{config.pdftohtml_path} -nofonts "{input_file_path}" "{output_file_path}"'
+            f'{self.config.pdftohtml_path} -nofonts "{input_file_path}" "{os.path.join(self.config.temp_file_path, output_file_path)}"'
         )
         return True
+
+    def PDF_to_DOCX(self, input_file_path, output_file_path):
+        pass
+
+    def DOCX_to_HTML(self, input_file_path, output_file_path):
+        pass
 
     def PPTX_to_PDF(self, input_file_path, output_file_path, formatType=32):
         powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
