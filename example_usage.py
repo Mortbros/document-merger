35762d4f025fe06c7094243fc47790424714784e
mortbros <sandromort@gmail.com>
2024-08-27 17:28:06 +1000

    Update example usage

--- example_usage.py
+++ example_usage.py
@@ -1,4 +1,7 @@
 from document_merger import DocumentMerger, DocumentMergerConfig
+
+# from src.document_merger import DocumentMerger, DocumentMergerConfig
+
 import os
 
 user_path = os.path.expanduser("~\\")
@@ -9,13 +12,27 @@ document_merger_config = DocumentMergerConfig(
     temp_file_path=f"{user_path}Downloads\\document-merger",
     file_path_map_path=f"{user_path}Downloads\\document-merger\\path_map.json",
     ocr_map_path=f"{user_path}Downloads\\document-merger\\ocr_map.json",
-    image_output_path=f"{user_path}Downloads\\document-merger\\images",
+    # image_output_path=f"{user_path}Downloads\\document-merger\\images",
     tesseract_path="C:\\Program files\\Tesseract-OCR\\tesseract.exe",
-    ignored_dirs=("Textbooks", "temp", "__pycache__"),
+    ignored_dirs=(
+        "Textbooks",
+        "temp",
+        "__pycache__",
+        "Assignment 1",
+        "Assessment 1",
+        "Assignment 2",
+        "Assessment 2",
+        "Algorithms and Analysis",
+        "Full stack development",
+        "Introduction to Cybersecurity",
+        "Software engineering fundamentals",
+    ),
     main_output_type="html",
     keep_temp_files=True,
     show_image=False,
     print_status_table=True,
+    create_imageless_version=True,
+    process_subdirectories_individually=True,
     absolute_temp_directory_names=True,
 )
 
